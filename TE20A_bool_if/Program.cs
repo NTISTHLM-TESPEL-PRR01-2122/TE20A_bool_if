bc3c3b001575b2d25fd29844edad1c101bcce140
Mikael Bergström <krank23@gmail.com>
2021-09-14 14:26:11 +0200

    Initial commit

---
+++ TE20A_bool_if/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+string t = "H";
+
+// bool isAlive = true;
+
+// Console.WriteLine(t == "V");
+
+Console.WriteLine("Vad heter du?");
+string name = Console.ReadLine();
+
+if (name == "micke")
+{
+  Console.WriteLine("YES");
+  // string x = "hej";
+  // if (x == "hej")
+  // {
+
+  // }
+}
+else if (name == "albin")
+{
+  Console.WriteLine("WHAT");
+}
+else
+{
+  Console.WriteLine("NO!");
+}
+
+
+Console.ReadLine();
